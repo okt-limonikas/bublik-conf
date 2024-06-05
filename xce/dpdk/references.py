82839539789221515884e94dc164f58b58b0998a
Danil Kostromin <danil.kostromin@arknetworks.am>
2024-06-05 19:58:57 +0300

    Initial commit

---
+++ xce/dpdk/references.py
@@ -0,0 +1,78 @@
+class References:
+    logs = {
+        "X": {
+            "name": "Jira",
+            "uri": [
+                ""
+            ],
+        },
+        "OL": {
+            "name": "OKTET Labs Bugzilla",
+            "uri": [
+                ""
+            ],
+        },
+        "LOGS_BASE": {
+            "name": "Logs base",
+            "uri": [
+                "https://ts-factory.io/logs/"
+            ],
+        },
+    }
+
+    revisions = {
+        "BUBLIK_REPO": {
+            "name": "Bublik backend repository",
+            "uri": [
+                "https://github.com/okt-limonikas/bublik"
+            ],
+        },
+        "BUBLIK_UI_REPO": {
+            "name": "Bublik frontend repository",
+            "uri": [
+                "https://github.com/okt-limonikas/bublik-ui"
+            ],
+        },
+        "DPDK_PMD_TS_REV": {
+            "name": "DPDK-PMD-TS",
+            "uri": [
+                ""
+            ],
+        },
+        "VSWITCH_TS_REV": {
+            "name": "vSwitch-TS",
+            "uri": [
+                ""
+            ],
+        },
+        "TSCONF_REV": {
+            "name": "TS-Conf",
+            "uri": [
+                ""
+            ],
+        },
+        "TE_REV": {
+            "name": "Test Environment",
+            "uri": [
+                ""
+            ],
+        },
+        "DPDK_GIT_COMMIT": {
+            "name": "dpdk-next-sfc",
+            "uri": [
+                ""
+            ],
+        },
+        "OVS_GIT_COMMIT": {
+            "name": "Open vSwitch",
+            "uri": [
+                ""
+            ],
+        },
+        "SFC_LINUX_NET_GIT_COMMIT": {
+            "name": "sfc-linux-net",
+            "uri": [
+                ""
+            ],
+        },
+    }
